2faea120f50fff2de98db466b02de613f9b7c619
DavidchatLC <david_lopez_cueto@outlook.com>
2022-02-06 22:16:14 -0600

    Practica 2: php poo v3 Pagina con libreria de composer

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php include 'includes/header.php';?>
 
 <?php
-require_once 'clases/claseCamion.php';
+require_once("vendor/autoload.php");
+require_once("clases/claseCamion.php");
+
 
 $bicicleta = new Vehiculo("Bicicleta",2,"Roja");
 $arrayVehiculo = $bicicleta -> getDatos();
 
-echo "<pre>";
-print_r($arrayVehiculo);
-echo "</pre>";
+d($arrayVehiculo);
 
 $auto = new Auto("Sentra",4 ,"Azul Rey", 4, "2.0");
 $arrayAuto = $auto -> getDatos();
 
-echo "<pre>";
-print_r($arrayAuto);
-echo "</pre>";
+
+d($arrayAuto);
+
 
 $camion = new Camion("Volvo 9800", 10, "Plata", 1, "D 13", 50);
 $camion -> setPantallas(25);
 $arrayCamion = $camion -> getDatos();
 
-echo "<pre>";
-print_r($arrayCamion);
-echo "</pre>";
+
+d($arrayCamion);
+
 
 include 'includes/footer.php';?>
