0fa10fc6e3d1c97366eaeede69e0eed4a569d0cf
DavidchatLC <david_lopez_cueto@outlook.com>
2022-02-06 22:10:54 -0600

    Practica 2: php poo v2 carpeta de composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '78485a91c714064b31e53c63accb773737b7c2cc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '78485a91c714064b31e53c63accb773737b7c2cc',
+            'dev_requirement' => false,
+        ),
+        'kint-php/kint' => array(
+            'pretty_version' => '4.1.1',
+            'version' => '4.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kint-php/kint',
+            'aliases' => array(),
+            'reference' => 'e64b939f9ceb9620abd982e2a66a3289fcf4e837',
+            'dev_requirement' => false,
+        ),
+    ),
+);
